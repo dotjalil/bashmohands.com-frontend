f04ac6f1b35832803808bcc4c7d61609deab013c
dotjalil <mgkmohammad@gmail.com>
2023-10-15 12:34:09 +0300

    auth task files and comments

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,16 @@
 export default function LoginPage() {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      <h1>Login Page</h1>
+      {/* TODO
+            1. Add login form: email address, password
+            2. Add signup link: redirect to /signup
+            3. Submit user's data via the action below.
+        */}
+    </>
+  );
+}
+
+export async function action({ request }) {
+  // TODO...
 }
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,23 +1,29 @@
 export default function SignupPage() {
-  return <h1>Signup Page</h1>;
+  return (
+    <>
+      {/* TODO
+            1. Add signup form: first name, last name, email address, password
+            2. Add login link: redirect to /login
+            3. Submit user's data via the action below.
+        */}
+    </>
+  );
 }
 
 export async function action({ request /** you'll need this */ }) {
   /**
-   * STEP 1.
-   * extract data from the user's request object
+   * What's an action function?
+   * Using simple words, an action is a function that fires automatically, by react-router-dom,
+   * when a non-get request, post/put/delete, is sent to your router.
+   *
+   * Suppose we want to submit the signup form to the server, you will
+   * need to handle a post request sent via your registeration form.
+   * You can use the action to submit these data.
    */
-  // example: const data = await request.formData(); // request is given as a prameter
-  // TODO...
   /**
-   * STEP 2.
-   * construct the response object
-   */
-  // example: const response = await fetch( backend_auth_endpoint, { method, headers: { }, body: JSON.stringify(auth_data), });
-  // TODO...
-  /**
-   * STEP 3.
-   * respond to unvalid requests
+   * STEP 1. extract data from the user's request object
+   * STEP 2. construct the response object
+   * STEP 3. respond to unvalid requests
    */
   /** The following lines will be uncommented after you implement the response object */
   //   if (response.status === 422 || response.status === 401) {
@@ -26,7 +32,6 @@ export async function action({ request /** you'll need this */ }) {
   //   if (!response.ok) {
   //     throw json({ message: "Could not authenticate user." }, { status: 500 });
   //   }
-  // TODO...
   /**
      * EXTRA
      * If you want to use search params inside this function,
